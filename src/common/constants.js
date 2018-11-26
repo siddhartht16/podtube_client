0b2d7a2080430f246e1eb683508ab5f6bdb7ef8b
Siddharth Talwekar <siddharthtalwekar@gmail.com>
2018-11-25 21:29:04 -0500

    Added additional methods

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,7 +2,19 @@ export const LISTEN_NOTES_BASE_URL = "https://listennotes.p.mashape.com/api/v1/"
 
 export const PODCAST_GENRES = "genres/";
 export const BEST_PODCASTS = "best_podcasts/";
+export const SEARCH = "search/";
+export const PODCASTS = "podcasts/";
 
-//Query params
-export const GENRE_ID = "genre_id";
-export const PAGE = "page";
+// Query params
+export const QP_GENRE_ID = "genre_id";
+export const QP_PAGE = "page";
+export const QP_SEARCH_TERM = "q";
+export const QP_SEARCH_TYPE_PODCAST = "type=podcast";
+export const QP_SEARCH_TYPE_EPISODE = "type=episode";
+export const QP_SEARCH_ONLY_IN_TITLE = "only_in=title";
+export const QP_NEXT_EPISODE_PUB_DATE = "next_episode_pub_date";
+export const QP_SORT = "sort";
+
+// Query param values
+export const QP_SORT_RECENT_VALUE = "recent_first";
+export const QP_SORT_OLDEST_VALUE = "oldest_first";
--- src/common/urls.js
+++ src/common/urls.js
@@ -7,5 +7,25 @@ export function get_genre_url() {
 export function get_genre_best_podcasts_url(p_genre_id, p_page_number){
 
     return `${constants.LISTEN_NOTES_BASE_URL}${constants.BEST_PODCASTS}?
-    ${constants.GENRE_ID}=${p_genre_id}&${constants.PAGE}=${p_page_number}`;
+    ${constants.QP_GENRE_ID}=${p_genre_id}&${constants.QP_PAGE}=${p_page_number}`;
+}
+
+export function get_search_by_title_url(p_search_term){
+
+    return `${constants.LISTEN_NOTES_BASE_URL}${constants.SEARCH}?
+    ${constants.QP_SEARCH_TERM}=${p_search_term}&${constants.QP_SEARCH_TYPE_PODCAST}
+    &=${constants.QP_SEARCH_ONLY_IN_TITLE}`;
+}
+
+export function get_podcast_episodes_url(p_podcast_id, p_next_episode_pub_date, p_sort_order){
+
+    const next_episode_pub_date_query_param = p_next_episode_pub_date ?
+        `${constants.QP_NEXT_EPISODE_PUB_DATE}=${p_next_episode_pub_date}` : "";
+
+    const sort_query_param = p_sort_order ?
+            `${constants.QP_SORT}=${p_sort_order}` :
+        `${constants.QP_SORT}=${constants.QP_SORT_RECENT_VALUE}`;
+
+    return `${constants.LISTEN_NOTES_BASE_URL}${constants.PODCASTS}/${p_podcast_id}?
+    ${next_episode_pub_date_query_param}&${sort_query_param}`;
 }
