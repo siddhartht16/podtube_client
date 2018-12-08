5636c2baedb3b3a783e5f0377fdacaa15f4d9e3d
sidco0014 <pasari.s@husky.neu.edu>
2018-12-08 11:46:37 -0500

    updated profile component

--- src/components/Auth/Profile.js
+++ src/components/Auth/Profile.js
@@ -5,14 +5,19 @@ export default class Profile extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            userProfile: null
+        }
     }
 
     componentDidMount() {
-        // UserService.fetchProfileForUser()
-        //     .then(data => {
-        //         console.log(data);
-        //     });
+        UserService.fetchProfileForUser()
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    userProfile: data
+                })
+            });
     }
 
     render() {
@@ -24,28 +29,25 @@ export default class Profile extends Component {
                         <div className="col-md-6">
                             <div className="profile-body">
                                 <p className="text-green">Profile</p>
-                                <form role="form">
-                                    <div className="form-group">
-                                        <label className="control-label">First name</label>
-                                        <p id="profile-firstname">Siddhant</p>
-                                    </div>
+                                {
+                                    this.state.userProfile === null ? <i>Loading...</i> :
+                                        <form role="form">
+                                            <div className="form-group">
+                                                <label className="control-label">First name</label>
+                                                <p id="profile-firstname">{this.state.userProfile.firstname}</p>
+                                            </div>
 
-                                    <div className="form-group">
-                                        <label className="control-label">Last name</label>
-                                        <p id="profile-lastname">Pasari</p>
-                                    </div>
+                                            <div className="form-group">
+                                                <label className="control-label">Last name</label>
+                                                <p id="profile-lastname">{this.state.userProfile.lastname}</p>
+                                            </div>
 
-                                    <div className="form-group">
-                                        <label className="control-label">Username</label>
-                                        <p id="profile-username">Sidco0014</p>
-                                    </div>
-
-                                    <div className="form-group">
-                                        <label className="control-label">Password</label>
-                                        <p id="profile-password">Password123</p>
-                                    </div>
-
-                                </form>
+                                            <div className="form-group">
+                                                <label className="control-label">Username</label>
+                                                <p id="profile-username">{this.state.userProfile.username}</p>
+                                            </div>
+                                        </form>
+                                }
                             </div>
                         </div>
                     </div>
