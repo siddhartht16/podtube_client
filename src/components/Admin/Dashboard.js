ac2bc5dee1aa9b2b291adba171cbb5fdb3fe061b
sidco0014 <pasari.s@husky.neu.edu>
2018-12-10 14:46:45 -0500

    Styling admin cards

--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom'
+import './Dashboard.style.css';
+
 export default class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -13,12 +15,34 @@ export default class Dashboard extends Component {
 
     render() {
         return (
-            <div>
-               <h3 className="mt-3">Welcome To PodTube Admin</h3>
-                <p>Users: </p>
-                <p>Categories: </p>
-                <p>Poscasts: </p>
-                <p>Episodes: </p>
+            <div className="admin-dashboard-wrapper">
+                <h3 className="mt-3">PodTube Admin</h3>
+                <div className="row mt-3">
+                    <div className="col-md-3">
+                        <div className="admin-card">
+                            <p><span className="fa fa-user icon"/></p>
+                            <p>Users: <span className="count">100</span></p>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="admin-card">
+                            <p><span className="fa fa-list icon"/></p>
+                            <p>Categories: <span className="count">300</span></p>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="admin-card">
+                            <p><span className="fa fa-podcast icon"/></p>
+                            <p>Podcasts: <span className="count">800</span></p>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="admin-card">
+                            <p><span className="fa fa-tv icon"/></p>
+                            <p>Episodes: <span className="count">10,000</span></p>
+                        </div>
+                    </div>
+                </div>
                 <br/>
                 <p><Link to="/admin/category-list">Categories</Link></p>
                 <p><Link to="/admin/podcast-list">Podcasts</Link></p>
